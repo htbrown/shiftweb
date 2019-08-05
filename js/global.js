e440b6f3e0427312f089816e61abb1b39876010c
Hayden <28675646+htbrown@users.noreply.github.com>
2019-08-05 18:33:56 +0200

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -1,27 +1,6 @@
 window.scroll({
-    top: 2500,
+    top: 0,
     left: 0,
     behavior: 'smooth'  // <pre rel="HTML"><code markup="tt" class="language-markup">
 });
 
-// Scroll certain amounts from current position
-window.scrollBy({
-    top: 100, // could be negative value
-    left: 0,
-    behavior: 'smooth'
-});
-
-// Scroll to a certain element
-document.querySelector('.hero').scrollIntoView({
-    behavior: 'smooth'
-});
-
-let modal = document.getElementById('modal');
-
-function openFeatures() {
-    modal.className = 'modal is-active';
-}
-
-function closeFeatures() {
-    modal.className = 'modal';
-}
